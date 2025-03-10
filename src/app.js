81c18acf6b67e79f664e8dd8d1ba9a42de90cff0
Andres Garcia <20233tn107@utez.edu.mx>
2025-03-09 19:04:47 -0600

    feat: Agregada la configuracion de las cors para recibir petiiones desde Web y Mobile

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
 connectDB();
 app.use(express.json());
 app.disable('X-Powered-By');
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:8081'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const routes = require('./routes');
 app.use('/api', routes);
